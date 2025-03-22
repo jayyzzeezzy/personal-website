df35c89d0dadbe0c29a7df62b0708c49ebe64d4e
James Zhou <jam9es@gmail.com>
2025-03-22 00:18:09 -0700

    Write out templates for navbar and intro sections

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,28 @@ function App() {
 
   return (
     <>
-      <h1>Hi, I'm James.</h1>
+      <nav>
+        <a href="/">
+          <button>JZ</button>
+        </a>
+        <a href="https://drive.google.com/file/d/1OgxCoq5m945q1StFRq1NP5mEGNymfCf1/view?usp=sharing" target="_blank">
+          <button type="button">Resume</button>
+        </a>
+      </nav>
+      
+      <div>
+        <h1>Hi, I'm James.</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        </p>
+        <p>
+        Lorem ipsum consectetur adipiscing elit.
+        </p>
+      </div>
+
+      <div>
+        <h1>Recent Projects</h1>
+      </div>
     </>
   )
 }
---
+++ src/db/myinfo.js
@@ -0,0 +1,5 @@
+const info = {
+    aboutMe: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed auctor sagittis orci a commodo. In semper mi in consequat fringilla. Ut iaculis ante quis cursus cursus. Morbi rhoncus lorem ut consectetur maximus. Nam nulla turpis, placerat id diam maximus, congue rhoncus dolor. Vestibulum sed eros vel nisi gravida maximus. Cras nisi sapien, varius vitae auctor vel, feugiat id risus. Vivamus nec lectus vitae mi convallis sodales. Fusce nisl est, elementum sit amet nisi sit amet, fringilla lacinia tellus. Nam eleifend consectetur nulla cursus euismod. Quisque vel pretium nulla. Aenean fringilla odio eu facilisis molestie. Donec aliquet sapien nisl, vitae commodo mi tristique et.",
+};
+
+export default info;
