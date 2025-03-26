3c1b15212a9b9bfe823d1d0443f6b5cad5a6a0ca
James Zhou <jam9es@gmail.com>
2025-03-25 19:46:40 -0700

    Write footer section and add temp styling

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Hero from './components/hero';
 import Project from './components/project';
 import TechStack from './components/techstack';
 import AboutMe from './components/aboutme';
+import Socials from './components/social';
 
 function App() {
 
@@ -14,6 +15,7 @@ function App() {
       <Project />
       <TechStack />
       <AboutMe />
+      <Socials />
     </>
   )
 }
---
+++ src/components/social.jsx
@@ -0,0 +1,44 @@
+function Socials() {
+    return (
+        <footer className="socials">
+            <h1 className="socials-heading">Socials</h1>
+            <div className="social-media">
+                <a href="mailto:jam9es@gmail.com" target="_blank" className="social-link">
+                    <span className="icon">
+                        <img src="src/assets/images/socials/gmail.svg" alt="gmail icon" />
+                    </span>
+                </a>
+                <a href="https://github.com/jayyzzeezzy" target="_blank" className="social-link">
+                    <span className="icon">
+                        <img src="src/assets/images/socials/github.svg" alt="github icon" />
+                    </span>
+                </a>
+                <a href="https://www.linkedin.com/in/jzfromkp/" target="_blank" className="social-link">
+                    <span className="icon">
+                        <img src="src/assets/images/socials/linkedin.svg" alt="linkedin icon" />
+                    </span>
+                </a>
+                <a href="https://instagram.com/jzfromkp" target="_blank" className="social-link">
+                    <span className="icon">
+                        <img src="src/assets/images/socials/instagram.svg" alt="instagram icon" />
+                    </span>
+                </a>
+                <a href="https://tiktok.com/@jayyzzeezy" target="_blank" className="social-link">
+                    <span className="icon">
+                        <img src="src/assets/images/socials/tiktok.svg" alt="tiktok icon" />
+                    </span>
+                </a>
+                <a href="https://www.reddit.com/u/jayyzzeezy/s/XDItpO79en" target="_blank" className="social-link">
+                    <span className="icon">
+                        <img src="src/assets/images/socials/reddit.svg" alt="reddit icon" />
+                    </span>
+                </a>
+            </div>
+            <div className="copyright">
+                <p>Handcrafted by me © {new Date().getFullYear()}</p>
+            </div>
+        </footer>
+    )
+}
+
+export default Socials;
