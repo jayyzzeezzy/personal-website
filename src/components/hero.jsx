bf17dd39c054b35a867c05c0767d6176623757e4
James Zhou <jam9es@gmail.com>
2025-03-26 22:29:04 -0700

    Write intro line and add margins for hero and project sections.

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,17 +1,20 @@
 function Hero() {
     return (
         <div>
-            <div>
+            <div className="profile-container">
                 <img src="/pfp.png" alt="profile picture" className="profile-picture"/>
             </div>
-            <div>
+            <div className="intro">
                 <h1>Hi, I'm James.</h1>
+                <div className="intro-line">
                 <h3>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    I am a self-started software engineer with a passion for
                 </h3>
-                <h3>
-                    Lorem ipsum consectetur adipiscing elit.
-                </h3>
+                <h3>making scalable, efficient, and elegant solutions.</h3>
+                </div>
+                <p>
+                    <span className="emoji">&#128640;</span> Coding the Future, One Line at a Time. 
+                </p>
             </div>
         </div>
     )
