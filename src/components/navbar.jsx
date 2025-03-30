92516e488f02694455c3dd50e70724bac4bc55de
James Zhou <jam9es@gmail.com>
2025-03-30 16:55:25 -0700

    Add new button in navbar and edit buttons in project section.

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -4,9 +4,15 @@ function Navbar() {
             <a href="/">
                 <button className="homebtn">JZhou</button>
             </a>
-            <a href="https://drive.google.com/file/d/1OgxCoq5m945q1StFRq1NP5mEGNymfCf1/view?usp=sharing" target="_blank">
-                <button className="resumebtn">Resume</button>
-            </a>
+            <div className="navbtn">
+                <a href="https://drive.google.com/file/d/1OgxCoq5m945q1StFRq1NP5mEGNymfCf1/view?usp=sharing" target="_blank">
+                    <button className="resumebtn">Resume</button>
+                </a>
+                <a href="#" target="_blank">
+                    <button className="qrbtn">QR Code</button>
+                </a>
+            </div>
+            
         </nav>
     )
 };
