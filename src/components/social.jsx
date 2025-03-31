ff6c7b34324eca81e8909f1cdd06ccadd6e26cae
James Zhou <jam9es@gmail.com>
2025-03-30 19:22:39 -0700

    Import icons directly in Socials component.

--- src/components/social.jsx
+++ src/components/social.jsx
@@ -1,3 +1,10 @@
+import gmail from "../assets/images/socials/gmail.svg";
+import github from "../assets/images/socials/github.svg";
+import linkedin from "../assets/images/socials/linkedin.svg";
+import instagram from "../assets/images/socials/instagram.svg" ;
+import tiktok from "../assets/images/socials/tiktok.svg";
+import reddit from "../assets/images/socials/reddit.svg";
+
 function Socials() {
     return (
         <footer className="socials">
@@ -5,32 +12,32 @@ function Socials() {
             <div className="social-media">
                 <a href="mailto:jam9es@gmail.com" target="_blank" className="social-link">
                     <span className="icon">
-                        <img src="src/assets/images/socials/gmail.svg" alt="gmail icon" />
+                        <img src={gmail} alt="gmail icon" />
                     </span>
                 </a>
                 <a href="https://github.com/jayyzzeezzy" target="_blank" className="social-link">
                     <span className="icon">
-                        <img src="src/assets/images/socials/github.svg" alt="github icon" />
+                        <img src={github} alt="github icon" />
                     </span>
                 </a>
                 <a href="https://www.linkedin.com/in/jzfromkp/" target="_blank" className="social-link">
                     <span className="icon">
-                        <img src="src/assets/images/socials/linkedin.svg" alt="linkedin icon" />
+                        <img src={linkedin} alt="linkedin icon" />
                     </span>
                 </a>
                 <a href="https://instagram.com/jzfromkp" target="_blank" className="social-link">
                     <span className="icon">
-                        <img src="src/assets/images/socials/instagram.svg" alt="instagram icon" />
+                        <img src={instagram} alt="instagram icon" />
                     </span>
                 </a>
                 <a href="https://tiktok.com/@jayyzzeezy" target="_blank" className="social-link">
                     <span className="icon">
-                        <img src="src/assets/images/socials/tiktok.svg" alt="tiktok icon" />
+                        <img src={tiktok} alt="tiktok icon" />
                     </span>
                 </a>
                 <a href="https://www.reddit.com/u/jayyzzeezy/s/XDItpO79en" target="_blank" className="social-link">
                     <span className="icon">
-                        <img src="src/assets/images/socials/reddit.svg" alt="reddit icon" />
+                        <img src={reddit} alt="reddit icon" />
                     </span>
                 </a>
             </div>
