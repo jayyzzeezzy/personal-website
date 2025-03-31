78e47c3ada2fe481686fbfcd51b50f1e5ccaeecf
James Zhou <jam9es@gmail.com>
2025-03-30 20:26:15 -0700

    Insert URL for Resume and QR code.

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,14 +1,16 @@
+import myInfo from '../db/myinfo';
+
 function Navbar() {
     return (
         <nav>
-            <a href="/">
+            <a href="https://jameszhou.netlify.app/">
                 <button className="homebtn">JZhou</button>
             </a>
             <div className="navbtn">
-                <a href="https://drive.google.com/file/d/1OgxCoq5m945q1StFRq1NP5mEGNymfCf1/view?usp=sharing" target="_blank">
+                <a href={myInfo.resume} target="_blank">
                     <button className="resumebtn">Resume</button>
                 </a>
-                <a href="#" target="_blank">
+                <a href={myInfo.qrCode} target="_blank">
                     <button className="qrbtn">QR Code</button>
                 </a>
             </div>
